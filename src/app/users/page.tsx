8d722cec3c2fc91c2704a8f4571fb9cf0d96faa7
arisa <98852332+Unonkgw@users.noreply.github.com>
2025-06-23 07:26:46 +0000

    feat: added student list

---
+++ src/app/users/page.tsx
@@ -0,0 +1,11 @@
+import UsersList from "@/components/ui/UsersList";
+
+function Page() {
+    <div className="w-screen h-screen flex items-center justify-center">
+        <h2 className="m-8 font-bold text-4">Users List</h2>
+        <UsersList />
+    </div> 
+        
+}
+
+export default Page;
--- src/components/ui/StudentsList.tsx
+++ src/components/ui/StudentsList.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 
+export type StudentType = {
+  studentId: string;
+  department: string;
+  email: string;
+  name: string;
+  role: "STUDENT" | "ADMIN";
+}
+
 const StudentsList = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/students`
   );
 
-  const students = await res.json();
+  const students: StudentType[] = await res.json();
 
-  return <div>{JSON.stringify(students)}</div>;
+  // return <div>{JSON.stringify(students)}</div>;
+
+  return (
+    <div>
+    {students.map((student, index) => {
+      return <p key={`${student}_${index}}`}>{student.name}</p>;
+    })}
+    </div>
+  );
 };
 
 export default StudentsList;
